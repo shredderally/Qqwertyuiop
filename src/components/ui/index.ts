36a04bc49dfbcf7e9603dab1309e36ef657413d1
shredderally <zechipsani9@gmail.com>
2026-08-02 19:46:51 +0000

    Create index.ts to export UI components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Button } from './Button'
+export { Input } from './Input'
+export { Label } from './Label'
+export { FieldError } from './FieldError'
+export { Card } from './Card'
+export { Badge } from './Badge'
+export { Spinner } from './Spinner'
